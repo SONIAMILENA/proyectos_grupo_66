e11906d38dc230e760f19b581d90b3f60318e9e7
SONIAMILENA <soniamilenam@gmail.com>
2021-07-05 19:57:20 +0200

    se agreganuevo proyecto

---
+++ src/Producto.java
@@ -0,0 +1,12 @@
+public class Producto {
+
+    private String nombre = "";
+    private float precio = 0;
+
+    public void mostrar() {
+        System.out.println(nombre + ":" + precio);
+
+    }
+
+    
+}
